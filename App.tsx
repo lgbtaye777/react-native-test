ab89d02be74fd9aa665c004382e54ea82fe2ed60
lgbtaye777 <m-fedurin@mail.ru>
2023-12-15 01:09:22 +0700

    Move Section to a separate component

--- App.tsx
+++ App.tsx
@@ -24,36 +24,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import { Section } from './src/Section';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
